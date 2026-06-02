4389e1b1b5e48d3125bb13b7ec68c7af8b5707f3
Willis Marques <willis.s.marques@gmail.com>
2026-06-02 20:24:04 -0300

    feat: implementar função assincrona de cadastro de motorista no firestore

--- src/app/shared/services/autenticacao.ts
+++ src/app/shared/services/autenticacao.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
 import { Firestore, doc, setDoc, collection } from '@angular/fire/firestore';
 import { Passageiro } from '../models/passageiro.model';
+import { Motorista } from '../models/motorista.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,10 +11,17 @@ export class Autenticacao {
   private autenticacao = inject(Auth);
   private firestore = inject(Firestore);
 
-  async criarPassageiro(dados: Passageiro, senha: string):Promise<void>{
+  async criarPassageiro(dados: Passageiro, senha: string): Promise<void> {
     const credencial = await createUserWithEmailAndPassword(this.autenticacao, dados.email, senha);
 
     const documentoReferencia = doc(this.firestore, 'passageiros', credencial.user.uid);
+    await setDoc(documentoReferencia, dados);
+  }
+
+  async criarMotorista(dados: Motorista, senha: string): Promise<void> {
+    const credencial = await createUserWithEmailAndPassword(this.autenticacao, dados.email, senha);
+
+    const documentoReferencia = doc(this.firestore, 'motoristas', credencial.user.uid);
     await setDoc(documentoReferencia, dados);
   }
 }
