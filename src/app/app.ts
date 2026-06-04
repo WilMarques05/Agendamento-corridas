1f9c8fde6c75218be94d993a43a440fd7089c954
Willis Marques <willis.s.marques@gmail.com>
2026-06-04 18:05:30 -0300

    feat: implementar validações visual no formulário de cadastro do passageiro utilizando control flow(controle de fluxo com @if)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CadastroPassageiro } from "./features/autenticacao/cadastro-passageiro/cadastro-passageiro";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CadastroPassageiro],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/features/autenticacao/cadastro-passageiro/cadastro-passageiro.ts
+++ src/app/features/autenticacao/cadastro-passageiro/cadastro-passageiro.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { idToken } from '@angular/fire/auth';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-cadastro-passageiro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro-passageiro.html',
   styleUrl: './cadastro-passageiro.css',
 })
-export class CadastroPassageiro {}
+export class CadastroPassageiro {
+  private formBuilder = inject(FormBuilder);
+  formulario = this.formBuilder.group({
+    nome: ['', [Validators.required]],
+    cpf: ['', [Validators.required, Validators.minLength(11)]],
+    telefone: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    senha: ['',[Validators.required, Validators.minLength(6)]]
+  });
+
+  cadastrar(){
+    if(this.formulario.valid){
+      console.log('Dados prontos para firebase:', this.formulario.value);
+    }else{
+      console.log('Formulário inválido! Verifique os campos.' );
+    }
+  }
+}
