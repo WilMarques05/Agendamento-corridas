20139e6e03d755a1883e11851da0a0fa8e364240
Willis Marques <willis.s.marques@gmail.com>
2026-06-08 22:42:26 -0300

    feat: iniciar estruturação iniciais de rotas utilizando lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'cadastro-passageiro',
+    loadComponent: () => import('./features/autenticacao/cadastro-passageiro/cadastro-passageiro').then(m => m.CadastroPassageiro)
+  },
+
+  {
+    path: 'cadastro-motorista',
+    loadComponent: () => import('./features/autenticacao/cadastro-motorista/cadastro-motorista').then(m => m.CadastroMotorista)
+  }
+];
