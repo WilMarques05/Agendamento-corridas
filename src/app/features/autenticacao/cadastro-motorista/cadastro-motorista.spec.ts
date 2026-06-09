011afe2347516586d2300134264e36c177ac6ce7
Willis Marques <willis.s.marques@gmail.com>
2026-06-08 21:00:40 -0300

    feat: criar estrutura para cadastro de motoristas, criar logica de formulário de cadastro de motoristas

---
+++ src/app/features/autenticacao/cadastro-motorista/cadastro-motorista.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CadastroMotorista } from './cadastro-motorista';
+
+describe('CadastroMotorista', () => {
+  let component: CadastroMotorista;
+  let fixture: ComponentFixture<CadastroMotorista>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CadastroMotorista],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CadastroMotorista);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/autenticacao/cadastro-motorista/cadastro-motorista.ts
@@ -0,0 +1,42 @@
+import { Component, inject, ChangeDetectorRef } from '@angular/core';
+import { Autenticacao } from '../../../shared/services/autenticacao';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro-motorista',
+  imports: [ReactiveFormsModule],
+  templateUrl: './cadastro-motorista.html',
+  styleUrl: './cadastro-motorista.css',
+})
+export class CadastroMotorista {
+  private formBuilder = inject(FormBuilder);
+  private authService = inject(Autenticacao);
+  private changeDetector = inject(ChangeDetectorRef);
+
+  formulario = this.formBuilder.group({
+    nomeCompleto: ['', [Validators.required]],
+    cpf: ['', [Validators.required, Validators.minLength(11)]],
+    cidade: ['', [Validators.required]],
+    telefone: ['', [Validators.required, Validators.minLength(11)]],
+    email: ['', [Validators.required, Validators.email]],
+    senha: ['', [Validators.required, Validators.minLength(6)]],
+    fotoPerfil: ['', [Validators.required]],
+    valorPorKm: ['', [Validators.required]],
+    modeloCarro: ['', [Validators.required]],
+    placaCarro: ['', [Validators.required, Validators.minLength(7)]],
+    corCarro: ['', [Validators.required]],
+    fotoCarroExterna: ['', [Validators.required]],
+    fotoCarroInterna: ['', [Validators.required]]
+  });
+
+  async cadastrar(){
+    if(this.formulario.valid){
+      const {senha, ...dadosSemSenha} = this.formulario.value;
+      await this.authService.criarMotorista(dadosSemSenha as any, senha as string);
+      console.log(`Motorista ${dadosSemSenha.nomeCompleto} cadastrado no Firebase.`);
+      this.formulario.reset();
+    }else{
+      console.log('Formulário inválido, verifique os campos.')
+    }
+  }
+}
