62a42e1963214b851c8aa7d9a16b60a3c96899a4
Willis Marques <willis.s.marques@gmail.com>
2026-06-14 21:22:24 -0300

    feat: adicionar rotas para landing-page e login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
+  {
+    path: '',
+    loadComponent: () => import('./features/home/landing-page/landing-page').then(m => m.LandingPage)
+  },
+
+  {
+    path: 'login',
+    loadComponent: () => import('./features/autenticacao/login/login').then(m => m.Login)
+  },
+
   {
     path: 'cadastro-passageiro',
     loadComponent: () => import('./features/autenticacao/cadastro-passageiro/cadastro-passageiro').then(m => m.CadastroPassageiro)
