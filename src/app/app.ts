383ee8de38b8e286a820d70220b7c2092bbec971
Willis Marques <willis.s.marques@gmail.com>
2026-06-21 21:55:36 -0300

    feat: adicionar component do header e footer ao app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CadastroPassageiro } from "./features/autenticacao/cadastro-passageiro/cadastro-passageiro";
+import { Header } from "./shared/components/header/header";
+import { Footer } from "./shared/components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CadastroPassageiro],
+  imports: [RouterOutlet, CadastroPassageiro, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
