33d5b2a1690dbfa70d3e015f1aa3a08381e29c30
WILLIS SILVA MARQUES <014.450864@universidadesenaicimatec.edu.br>
2026-07-08 09:48:16 -0300

    feat: adicionar componente de carrossel na landing page

--- src/app/features/home/landing-page/landing-page.ts
+++ src/app/features/home/landing-page/landing-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../../../shared/components/header/header';
+import { Carrossel } from '../carrossel/carrossel';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Header],
+  imports: [Header, Carrossel],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
