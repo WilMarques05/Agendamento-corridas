f4bf004f26c3b19e52c9ab8e1a0f64c57cf39025
Willis Marques <willis.s.marques@gmail.com>
2026-07-16 18:59:27 -0300

    feat: implementar rota de direcionamento para tela de cadastro nos botões sou motorista e sou passageiro

--- src/app/features/home/landing-page/secao-hero/secao-hero.ts
+++ src/app/features/home/landing-page/secao-hero/secao-hero.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-secao-hero',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './secao-hero.html',
   styleUrl: './secao-hero.css',
 })
